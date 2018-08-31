881ef1278a02a752404c51e4fdcedcebb420e860
Bintang Piaggi Putra <bintang.piaggi44@gmail.com>
2018-08-31 14:14:45 +0700

    add Feature component

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Judul from './component/judul';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,6 +22,7 @@ export default class App extends Component<Props> {
   render() {
     return (
 	 <View style={styles.container}>
+        <Judul/>
         <Text style={styles.welcome}>Welcome!</Text>
         <Text style={styles.instructions}>Bintang Piaggi Putra</Text>
         <Text style={styles.instructions}>Kelas XI RPL 1 </Text>
